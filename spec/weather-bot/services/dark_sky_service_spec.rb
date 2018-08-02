ee93a5ae12d37594d265cde58596e4d3ce6ca156
bschwartz10 <bschwartz92@gmail.com>
2018-08-02 17:44:46 -0400

    Add test for get_icon method

--- spec/weather-bot/services/dark_sky_service_spec.rb
+++ spec/weather-bot/services/dark_sky_service_spec.rb
@@ -4,14 +4,23 @@ describe DarkSkyService do
 
   describe "todays weather" do
     it "returns todays weather details" do
-      # VCR.use_cassette("services/get_todays_weather") do
+      VCR.use_cassette("services/get_todays_weather") do
         weather = DarkSkyService.new.get_todays_weather(:new_york)
 
         expect(weather).to be_a(Hash)
         expect(weather).to have_key(:summary)
         expect(weather).to have_key(:icon)
         expect(weather).to have_key(:temperature)
-      # end
+      end
+    end
+  end
+
+  describe "weather icon" do
+    it "returns the corresponding emoji for given weather icon" do
+      dss = DarkSkyService.new
+      weather = { :icon => "rain" }
+
+      expect(dss.get_icon(weather[:icon])).to eq(':rain_cloud:')
     end
   end
 end
